3f86f3a8e42e7dcb881f88c76e713c1b8687cf94
Renato Oliveira <renatog.oliveira17@gmail.com>
2024-10-12 11:21:53 -0300

    entity Pessoa created; using lombok

--- strategy/src/main/java/renato/designpatterns/strategy/domain/Pessoa.java
+++ strategy/src/main/java/renato/designpatterns/strategy/domain/Pessoa.java
@@ -6,8 +6,18 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 @Entity
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+@NoArgsConstructor
 public class Pessoa {
 
 	@Id
@@ -17,6 +27,13 @@ public class Pessoa {
 	private String cpf;
 	private String email;
 	private LocalDate dataNascimento;
-	
-	
+
+	public Pessoa(String nome, String cpf, String email, LocalDate dataNascimento) {
+		super();
+		this.nome = nome;
+		this.cpf = cpf;
+		this.email = email;
+		this.dataNascimento = dataNascimento;
+	}
+
 }
