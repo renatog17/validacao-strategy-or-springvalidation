40b4932a962f7e535c937456c95ba44704ed9790
Renato Oliveira <renatog.oliveira17@gmail.com>
2024-10-12 12:16:44 -0300

    setup of teste profile

---
+++ strategy/src/test/java/renato/designpatterns/strategy/controller/PessoaControllerTest.java
@@ -0,0 +1,17 @@
+package renato.designpatterns.strategy.controller;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
+
+@AutoConfigureMockMvc
+@SpringBootTest
+@ActiveProfiles("test")
+@Transactional
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+public class PessoaControllerTest {
+
+	
+}
