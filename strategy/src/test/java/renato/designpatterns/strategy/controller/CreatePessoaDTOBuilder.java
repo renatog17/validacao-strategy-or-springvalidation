fa5ba241e7bfa87ce005bbd788c66d620354bd82
Renato Oliveira <renatog.oliveira17@gmail.com>
2024-10-12 12:51:08 -0300

    CreatePessoaDTOBuilder added

---
+++ strategy/src/test/java/renato/designpatterns/strategy/controller/CreatePessoaDTOBuilder.java
@@ -0,0 +1,97 @@
+package renato.designpatterns.strategy.controller;
+
+import java.time.LocalDate;
+
+public class CreatePessoaDTOBuilder {
+
+	private String nome;
+	private String cpf;
+	private String email;
+	private LocalDate dataNascimento;
+
+	public CreatePessoaDTOBuilder() {
+		this.nome = "Renato Default";
+		this.cpf = "12345678909";
+		this.email = "renato.default@example.com";
+		this.dataNascimento = LocalDate.of(2000, 12, 31);
+	}
+
+	// nome
+	public CreatePessoaDTOBuilder withNome(String nome) {
+		this.nome = nome;
+		return this;
+	}
+
+	public CreatePessoaDTOBuilder withNomeNull() {
+		this.nome = null;
+		return this;
+	}
+
+	public CreatePessoaDTOBuilder withNomeEmpty() {
+		this.nome = "";
+		return this;
+	}
+
+	// cpf
+	public CreatePessoaDTOBuilder withCPF(String cpf) {
+		this.cpf = cpf;
+		return this;
+	}
+
+	public CreatePessoaDTOBuilder withCPFNull() {
+		this.cpf = null;
+		return this;
+	}
+
+	public CreatePessoaDTOBuilder withCPFInvalid() {
+		this.nome = "12345678910";
+		return this;
+	}
+
+	// email
+	public CreatePessoaDTOBuilder withEmail(String email) {
+		this.email = email;
+		return this;
+	}
+
+	public CreatePessoaDTOBuilder withEmailNull() {
+		this.email = null;
+		return this;
+	}
+
+	public CreatePessoaDTOBuilder withEmailEmpty() {
+		this.email = "";
+		return this;
+	}
+
+	public CreatePessoaDTOBuilder withEmailInvalid() {
+		this.email = "thisisntanemail";
+		return this;
+	}
+
+	// data nascimento
+	public CreatePessoaDTOBuilder withDataNascimento(LocalDate dataNascimento) {
+		this.dataNascimento = dataNascimento;
+		return this;
+	}
+
+	public CreatePessoaDTOBuilder withDataNascimentoNull() {
+		this.dataNascimento = null;
+		return this;
+	}
+
+	public CreatePessoaDTOBuilder withDataNascimentoInThePresent() {
+		this.dataNascimento = LocalDate.now();
+		return this;
+	}
+
+	public CreatePessoaDTOBuilder withDataNascimentoInTheFuture() {
+		this.dataNascimento = LocalDate.now().plusDays(10);
+		return this;
+	}
+
+	public CreatePessoaDTOBuilder withDataNascimentoInThePast() {
+		this.dataNascimento = LocalDate.now().minusDays(10);
+		return this;
+	}
+}
--- strategy/src/test/java/renato/designpatterns/strategy/controller/PessoaControllerTest.java
+++ strategy/src/test/java/renato/designpatterns/strategy/controller/PessoaControllerTest.java
@@ -1,5 +1,6 @@
 package renato.designpatterns.strategy.controller;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
@@ -13,5 +14,54 @@ import org.springframework.transaction.annotation.Transactional;
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 public class PessoaControllerTest {
 
+	@Test
+	public void testPostPessoa() {
+		
+	}
+	//Name
+	@Test
+	public void testPostPessoaWithEmptyName() {
+		
+	}
+	
+	@Test
+	public void testPostPessoaWithNullName() {
+		
+	}
+	//CPF
+	@Test
+	public void testPostPessoaWithEmptyCPF() {
+		
+	}
+	@Test
+	public void testPostPessoaWithNullCPF() {
+		
+	}
+	@Test
+	public void testPostPessoaWithInvalidCPF() {
+		
+	}
+	//Email
+	@Test
+	public void testPostPessoaWithEmptyEmail() {
+		
+	}
+	@Test
+	public void testPostPessoaWithNullEmail() {
+		
+	}
+	@Test
+	public void testPostPessoaWithInvalidEmail() {
+		
+	}
+	//DataNascimento
+	@Test
+	public void testPostPessoaWithNullDataNascimento() {
+		
+	}
+	@Test
+	public void testPostPessoaWithFutureDataNascimento() {
+		
+	}
 	
 }
